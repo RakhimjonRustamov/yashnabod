38f794f981fa2f5d264041f3946bdf56b1377517
Rahimjon <r.rustamov@student.inha.uz>
2017-12-22 18:42:30 +0500

    America new

--- yash_techno.sql
+++ yash_techno.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Дек 22 2017 г., 09:04
+-- Время создания: Дек 22 2017 г., 14:41
 -- Версия сервера: 10.1.25-MariaDB
 -- Версия PHP: 7.1.7
 
@@ -38,14 +38,6 @@ CREATE TABLE `counters` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Дамп данных таблицы `counters`
---
-
-INSERT INTO `counters` (`id`, `counter1`, `counter2`, `counter3`, `counter4`, `created_at`, `updated_at`) VALUES
-(1, 121, 323, 252, 520, '2017-12-18 06:37:16', '2017-12-18 06:37:16'),
-(2, 252, 252, 565, 855, '2017-12-18 06:38:12', '2017-12-18 06:38:12');
-
 -- --------------------------------------------------------
 
 --
@@ -62,17 +54,6 @@ CREATE TABLE `employees` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Дамп данных таблицы `employees`
---
-
-INSERT INTO `employees` (`id`, `full_name_uz`, `full_name_ru`, `position_uz`, `position_ru`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'fowefib', 'fuhebwhfb', 'efjkwnfjin', 'fnewhbn', '1513533514.png', '2017-12-17 12:58:34', '2017-12-17 12:58:34'),
-(2, 'Ravshan', 'Makhmadaliev', 'Manager', 'Manager', '1513921211.jpg', '2017-12-22 00:40:12', '2017-12-22 00:40:12'),
-(3, 'Ravshan', 'Makhmadaliev', 'Manager', 'Manager', '1513921212.jpg', '2017-12-22 00:40:13', '2017-12-22 00:40:13'),
-(4, 'Sunnat', 'Anvarov', 'Bot', 'Bot', '1513921261.jpg', '2017-12-22 00:41:01', '2017-12-22 00:41:01'),
-(5, 'Ibrokhim', 'Movlonov', 'IOS developer', 'IOS developer', '1513921317.jpg', '2017-12-22 00:41:57', '2017-12-22 00:41:57');
 
 -- --------------------------------------------------------
 
@@ -100,7 +81,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (7, '2017_12_09_150612_create_offers_table', 1),
 (8, '2017_12_11_052623_create_employees_table', 1),
 (9, '2017_12_13_062203_create_popularities_table', 1),
-(10, '2017_12_18_112104_create_counters_table', 2);
+(10, '2017_12_18_112104_create_counters_table', 1);
 
 -- --------------------------------------------------------
 
@@ -121,15 +102,6 @@ CREATE TABLE `offers` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Дамп данных таблицы `offers`
---
-
-INSERT INTO `offers` (`id`, `name`, `surname`, `position`, `organization`, `region`, `email`, `phone`, `documents`, `created_at`, `updated_at`) VALUES
-(1, 'Rahimjon', 'Rustamov', 'Manager', 'INHA', '15', 'rakhimjon.rustamov@gmail.com', '973259200', '1513921662.zip', '2017-12-22 00:47:42', '2017-12-22 00:47:42'),
-(2, 'fgew', 'fewf', 'few', 'efwf', '15', 'rakhimjon.rustamov@gmail.com', '973259200', '1513921682.zip', '2017-12-22 00:48:02', '2017-12-22 00:48:02'),
-(3, 'hdbfgh', 'feg', 'fegwg', 'fewfwe', '18', 'rakhimjon.rustamov@gmail.com', '973259200', '1513921843.zip', '2017-12-22 00:50:43', '2017-12-22 00:50:43');
 
 -- --------------------------------------------------------
 
@@ -159,13 +131,6 @@ CREATE TABLE `popularities` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Дамп данных таблицы `popularities`
---
-
-INSERT INTO `popularities` (`id`, `link`, `popular_info_uz`, `popular_info_ru`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'fwehbfhb', 'fjkwefbhebw', 'nebnwhbghb', '1513533501.png', '2017-12-17 12:58:21', '2017-12-17 12:58:21');
-
 -- --------------------------------------------------------
 
 --
@@ -183,14 +148,6 @@ CREATE TABLE `posts` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Дамп данных таблицы `posts`
---
-
-INSERT INTO `posts` (`id`, `slug`, `image`, `title_uz`, `title_ru`, `body_uz`, `body_ru`, `created_at`, `updated_at`) VALUES
-(1, 'hello-new-features', '1513531325.png', 'Hello new features', 'Hello', 'fwefwe', 'fwefwef', '2017-12-17 12:22:05', '2017-12-17 12:22:05'),
-(2, 'hello', '1513739711.jpg', 'Hello', 'Hello baby', 'I am gonne kick your ass', 'I am gonne kick your ass', '2017-12-19 22:15:11', '2017-12-19 22:15:11');
 
 -- --------------------------------------------------------
 
@@ -214,15 +171,14 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `product_name_uz`, `product_name_ru`, `product_info_uz`, `product_info_ru`, `product_image`, `created_at`, `updated_at`) VALUES
-(1, 'k fk', 'j fwej fj`', 'fjkwenf', 'fbwejhbgwb', '1513533591.png', '2017-12-17 12:59:51', '2017-12-17 12:59:51'),
-(2, 'Comp', 'Comp', 'Comp', 'Comp', '1513737605.png', '2017-12-19 21:40:06', '2017-12-19 21:40:06'),
-(3, 'Notebook', 'Notebook', 'Notebook', 'Notebook', '1513737625.png', '2017-12-19 21:40:26', '2017-12-19 21:40:26'),
-(4, 'Notebook2', 'Notebook2', 'Notebook2', 'Notebook2', '1513737647.png', '2017-12-19 21:40:47', '2017-12-19 21:40:47'),
-(5, 'Noteboo3', 'Noteboo3', 'Noteboo3', 'Noteboo3', '1513737661.png', '2017-12-19 21:41:02', '2017-12-19 21:41:02'),
-(6, 'Noteboo4', 'Noteboo4', 'Noteboo4', 'Noteboo4', '1513737673.png', '2017-12-19 21:41:14', '2017-12-19 21:41:14'),
-(7, 'Noteboo5', 'Noteboo5', 'Noteboo5', 'Noteboo5', '1513737689.png', '2017-12-19 21:41:30', '2017-12-19 21:41:30'),
-(8, 'Noteboo5', 'Noteboo5', 'Noteboo5', 'Noteboo5', '1513737700.png', '2017-12-19 21:41:40', '2017-12-19 21:41:40'),
-(9, 'Noteboo5', 'Noteboo5', 'Noteboo5', 'Noteboo5', '1513737816.jpg', '2017-12-19 21:43:36', '2017-12-19 21:43:36');
+(1, 'New Techno', 'Hello New', 'This is new product', 'These are new', '1513949895.jpg', '2017-12-22 08:38:15', '2017-12-22 08:38:15'),
+(2, 'Asalari', 'Yangi', 'Hello is the new things', 'Jojo malo', '1513949921.jpg', '2017-12-22 08:38:41', '2017-12-22 08:38:41'),
+(3, 'This is new web application', 'Mana new', 'This is new koko', 'lolop popo', '1513949949.jpg', '2017-12-22 08:39:09', '2017-12-22 08:39:09'),
+(4, 'Web online magazine', 'Web online magazin', 'For you I am gonna tell', 'This is new change', '1513949986.jpg', '2017-12-22 08:39:46', '2017-12-22 08:39:46'),
+(5, 'Polo molo new', 'jojo lopo helo', 'Hello new is the koko', 'lolo polo kolo', '1513950017.jpg', '2017-12-22 08:40:17', '2017-12-22 08:40:17'),
+(6, 'But darling For', 'I am gonna learn', 'hferwbgwbh', 'nfhebhfb', '1513950039.jpg', '2017-12-22 08:40:39', '2017-12-22 08:40:39'),
+(7, 'Apachi', 'webser', 'W iefw f', 'fejkwbfubqk', '1513950056.jpg', '2017-12-22 08:40:56', '2017-12-22 08:40:56'),
+(8, 'fewjgwhiebg', 'few gbwjhq', 'fewfbqwbvj', 'fewbfubwehfb', '1513950065.jpg', '2017-12-22 08:41:05', '2017-12-22 08:41:05');
 
 -- --------------------------------------------------------
 
@@ -241,13 +197,6 @@ CREATE TABLE `receptions` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Дамп данных таблицы `receptions`
---
-
-INSERT INTO `receptions` (`id`, `full_name`, `date_appointment`, `phone`, `email`, `project_info`, `created_at`, `updated_at`) VALUES
-(1, 'fjnejqwnf', '2017-12-14 00:00:00', '981231223', 'rakhimjon.rustamov@gmail.com', 'Hello new things', '2017-12-17 13:02:56', '2017-12-17 13:02:56');
-
 -- --------------------------------------------------------
 
 --
@@ -264,14 +213,6 @@ CREATE TABLE `residents` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Дамп данных таблицы `residents`
---
-
-INSERT INTO `residents` (`id`, `resident_name_uz`, `resident_name_ru`, `resident_info_uz`, `resident_info_ru`, `resident_logo`, `created_at`, `updated_at`) VALUES
-(1, 'Rahimjon', 'Rustamov', 'Hello Uzbekistan', 'Hello Uzbekistan', '1513530925.png', '2017-12-17 12:15:26', '2017-12-17 12:15:26'),
-(2, 'hello baby', 'hello I am here', 'So we here', 'Koko lolo pop', '1513530950.png', '2017-12-17 12:15:50', '2017-12-17 12:15:50');
 
 -- --------------------------------------------------------
 
@@ -294,7 +235,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Rahimjon', 'rakhimjon.rustamov@gmail.com', '$2y$10$V3OBPGhPxUsL8sWeYKmkCu1wxr9KKLShJOh.hjMLi6N5aY9cenqkK', 'Q3kcrCCLzLmvE90szneJwFVFDVdpUy8TLZgWG9Ar4bFoZCOC31aflkAJ4cit', '2017-12-17 12:14:54', '2017-12-17 12:14:54');
+(1, 'Rahimjon', 'rakhimjon.rustamov@gmail.com', '$2y$10$7hHlRMsysKv0QvPSvLGigOHecH6RvYRLZ143VjyzXpi4apvDxoB6e', NULL, '2017-12-22 08:36:40', '2017-12-22 08:36:40');
 
 --
 -- Индексы сохранённых таблиц
@@ -376,12 +317,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `counters`
 --
 ALTER TABLE `counters`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `migrations`
 --
@@ -391,32 +332,32 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT для таблицы `offers`
 --
 ALTER TABLE `offers`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `popularities`
 --
 ALTER TABLE `popularities`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT для таблицы `receptions`
 --
 ALTER TABLE `receptions`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `residents`
 --
 ALTER TABLE `residents`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
