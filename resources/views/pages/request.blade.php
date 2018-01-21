87d9f8702b4e407f425f227806199d590ba720be
Омонжон <Omonjon96@mail.ru>
2018-01-21 15:23:00 +0500

    Ariza yuborish

--- resources/views/pages/request.blade.php
+++ resources/views/pages/request.blade.php
@@ -6,10 +6,10 @@
 
 @section('content')
 @section('page-title')
-<h1>Подать заявку</h1>
+<h1>@lang('request.Подать заявку')</h1>
 @endsection
     <div class="container-fluid dotted" id="application-container-fluid1">
-        <h1>Подать заявку</h1>
+        <h1>@lang('request.Подать заявку')</h1>
         <div class="container" id="container">
             <div class="row">
                 <form action="{{route('offer.request')}}" method="POST" enctype="multipart/form-data">
@@ -19,52 +19,52 @@
                         <div class="img-request">
                             <img src="https://muic.uz/uploads/pages/31a31e35a9347c92ec7cb35a83d01cf1.png" alt="">
                         </div>
-                        <h4>Представьтесь</h4>
+                        <h4>@lang('request.Представьтесь')</h4>
                         <div class="form-group">
-                            <input required="required" class="form-control" placeholder="Имя " type="text" id="first_name" name="name">
+                            <input required="required" class="form-control" placeholder="@lang('request.Имя') " type="text" id="first_name" name="name">
                             <div class="bar"></div>
                         </div>
                         <div class="form-group">
-                            <input required="required" class="form-control" placeholder="Фамилия " type="text" id="sur_name" name="surname">
+                            <input required="required" class="form-control" placeholder="@lang('request.Фамилия') " type="text" id="sur_name" name="surname">
                             <div class="bar"></div>
                         </div>
                         <div class="form-group">
-                            <input required="required" class="form-control" placeholder="Название должности " type="text" id="position_name" name="position">
+                            <input required="required" class="form-control" placeholder="@lang('request.Название должности') " type="text" id="position_name" name="position">
                             <div class="bar"></div>
                         </div>
                         <div class="form-group">
-                            <input required="required" class="form-control" placeholder="Организация " type="text" id="organisation" name="organization">
+                            <input required="required" class="form-control" placeholder="@lang('request.Организация') " type="text" id="organisation" name="organization">
                             <div class="bar"></div>
                         </div>
                         <div class="form-group">
                             <select name="region" id="ap_region" class="form-control" required="required">
-                                <option value="">Регион</option>
-                                <option value="6" id="6">Ташкент</option>
-                                <option value="7" id="7">Ташкентская область </option>
-                                <option value="9" id="9">Андижанская область</option>
-                                <option value="10" id="10">Бухарская область</option>
-                                <option value="11" id="11">Джизакская область</option>
-                                <option value="12" id="12">Кашкадарьинская область</option>
-                                <option value="14" id="14">Навоийская область</option>
-                                <option value="15" id="15">Наманганская область</option>
-                                <option value="16" id="16">Самаркандская область</option>
-                                <option value="17" id="17">Сурхандарьинская область</option>
-                                <option value="18" id="18">Сырдарьинская область</option>
-                                <option value="19" id="19">Ферганская область</option>
-                                <option value="20" id="20">Хорезмская область</option>
-                                <option value="21" id="21">Республика Каракалпакстан</option>
+                                <option value="">@lang('request.Регион')</option>
+                                <option value="6" id="6">@lang('request.Ташкент')</option>
+                                <option value="7" id="7">@lang('request.Ташкентская область')</option>
+                                <option value="9" id="9">@lang('request.Андижанская область')</option>
+                                <option value="10" id="10">@lang('request.Бухарская область')</option>
+                                <option value="11" id="11">@lang('request.Джизакская область')</option>
+                                <option value="12" id="12">@lang('request.Кашкадарьинская область')</option>
+                                <option value="14" id="14">@lang('request.Навоийская область')</option>
+                                <option value="15" id="15">@lang('request.Наманганская область')</option>
+                                <option value="16" id="16">@lang('request.Самаркандская область')</option>
+                                <option value="17" id="17">@lang('request.Сурхандарьинская область')</option>
+                                <option value="18" id="18">@lang('request.Сырдарьинская область')</option>
+                                <option value="19" id="19">@lang('request.Ферганская область')</option>
+                                <option value="20" id="20">@lang('request.Хорезмская область')</option>
+                                <option value="21" id="21">@lang('request.Республика Каракалпакстан')</option>
                             </select>
                             <div class="bar"></div>
                         </div>
                     </div>
                     <div class="bgwhite">
-                        <h4>Контакты</h4>
+                        <h4>@lang('request.Контакты')</h4>
                         <div class="form-group">
-                            <input required="required" class="form-control" type="email" id="email" placeholder="Э-почта" name="email">
+                            <input required="required" class="form-control" type="email" id="email" placeholder="@lang('request.Э-почта')" name="email">
                             <div class="bar"></div>
                         </div>
                         <div class="form-group phone_num">
-                            <label class="label">Номер телефона</label>
+                            <label class="label">@lang('request.Номер телефона')</label>
                             <div class="numcode">+998</div>
                             <input class="form-control" type="tel" id="phone" name="phone"  required="required"/>
                             <div class="bar "></div>
@@ -72,18 +72,18 @@
                     </div>
                 </div>
                 <div class="col-md-6" id="request-rule">
-                    <p class="text-justify"><b>Для получения статуса резидента инновационного технопарка “Яшнабад”, инициаторами инновационных проектов представляюстя в Дирекцию инновационного технопарка “Яшнабад” следующие документы:
+                    <p class="text-justify"><b>@lang('request.Для получения статуса резидента инновационного технопарка “Яшнабад”, инициаторами инновационных проектов представляюстя в Дирекцию инновационного технопарка “Яшнабад” следующие документы:')
                         </b></p>
                     <ul>
-                        <li>Паспорт инновационного проекта</li>
-                        <li>Аннотация к инновационному проекту</li>
-                        <li>Презентация инновационного проекта</li>
-                        <li>Заявления на получение статуса резидента</li>
+                        <li>@lang('request.Паспорт инновационного проекта')</li>
+                        <li>@lang('request.Аннотация к инновационному проекту')</li>
+                        <li>@lang('request.Презентация инновационного проекта')</li>
+                        <li>@lang('request.Заявления на получение статуса резидента')</li>
                     </ul>
                     <div class="col-md-12">
                         <div class="link ">
                             <a class="form-control form-group btn-primary download " href="{{url('/download')}}">
-                                <i class="fa fa-download " aria-hidden="true "></i> Скачать файлы (шаблоны) для отправки заявок
+                                <i class="fa fa-download " aria-hidden="true "></i> @lang('request.Скачать файлы (шаблоны) для отправки заявок')
                             </a>
                         </div>
                     </div>
@@ -91,7 +91,7 @@
                         <div class="input-group">
                             <label class="input-group-btn">
                                 <span class="btn btn-primary"><i class="fa fa-folder-open" aria-hidden="true"></i>
-                                    Browse<input type="file" name="documents" style="display: none;" multiple>
+                                    @lang('request.Browse')<input type="file" name="documents" style="display: none;" multiple>
                                 </span>
                             </label>
                             <input type="text" class="form-control" readonly>
@@ -99,7 +99,7 @@
                     </div>
                     @include('partials._message')
 
-                <button type="submit" class="button send_req"><i class="fa fa-paper-plane-o" aria-hidden="true"></i> Отправить заявку</button>
+                <button type="submit" class="button send_req"><i class="fa fa-paper-plane-o" aria-hidden="true"></i> @lang('request.Отправить заявку')</button>
 
 
                 </div>
