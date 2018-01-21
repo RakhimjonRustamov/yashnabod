7f26df7fe8e7d7baefd9179ec7626a03a82cd4ca
Sultonbek <s.ikromov@student.inha.uz>
2018-01-21 20:01:14 +0500

    translation by Sultan3.16

--- resources/views/pages/copyright.blade.php
+++ resources/views/pages/copyright.blade.php
@@ -6,18 +6,13 @@
 @endsection
 @section('content')
 @section('page-title')
-<h1>Защита авторских прав на объекты<br>интеллектуальной собственности</h1>
+<h1>{!!__('copyright.title-copy')!!}</h1>
 @endsection
 
     <div class="container-fluid container-fluid-copyright dotted" >
         <div class="container" >
-            <h1 class="title">Защита авторских прав на объекты интеллектуальной собственности</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h1 class="title">{!!__('copyright.title-copy')!!}</h1>
+            <p>{!!__('copyright.text-copy')!!}</p>
         </div>
     </div>
 
--- resources/views/pages/promotion.blade.php
+++ resources/views/pages/promotion.blade.php
@@ -6,18 +6,23 @@
 @endsection
 @section('content')
 @section('page-title')
-<h1>Содействие в прохождении<br>разрешительных процедур</h1>
+<h1>{!!__('promotion.title-prom')!!}</h1>
 @endsection
 
     <div class="container-fluid container-fluid-promotion dotted" >
         <div class="container" >
-            <h1 class="title">Содействие в прохождении разрешительных процедур </h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h1 class="title">{!!__('promotion.title-prom')!!}</h1>
+
+                <p>{!!__('promotion.text-prom1')!!}</p>
+                <ul>
+                    <li class="interval-promotion">{!!__('promotion.text-prom1.1')!!}</li>
+                    <li class="interval-promotion">{!!__('promotion.text-prom1.2')!!}</li>
+                    <li class="interval-promotion">{!!__('promotion.text-prom1.3')!!}</li>
+                    <li class="interval-promotion">{!!__('promotion.text-prom1.4')!!}</li>
+                    <li class="interval-promotion">{!!__('promotion.text-prom1.5')!!}</li>
+                    <li class="interval-promotion">{!!__('promotion.text-prom1.')!!}</li>
+                </ul>
+
         </div>
     </div>
 @endsection
