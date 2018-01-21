0d0e69990b79f5641da86b586c1c15cbcd0db6a5
Омонжон <Omonjon96@mail.ru>
2018-01-21 13:25:42 +0500

    Language

---
+++ resources/lang/en/reestr.php
@@ -0,0 +1,13 @@
+<?php
+
+return[
+	'title'=>" ",
+	'Реестер'=>" ",
+	'Reyestr raqami'=>"",
+	'Mulk Shakli'=>"",
+	'Yuridik Shaxs nomi'=>"",
+	'IFUT kodi'=>"",
+	'Pochta Manzili'=>"",
+	'E-mail'=>"",
+	'Kiritilgan Sana'=>""
+];
